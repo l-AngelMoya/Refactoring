deb5d692e3435a4fc1cec375094bd943c78950bb
l-AngelMoya <langel_andiyaro@hotmail.com>
2018-08-05 22:35:04 -0500

    Switch Statements

---
+++ SwitchStatements/src/SinCodeSmell/Animal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SinCodeSmell;
+
+/**
+ *
+ * @author Angel Moya
+ */
+public class Animal {
+    private String nombre;
+    
+    public void dormir(){
+    
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "nombre=" + nombre + '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
---
+++ SwitchStatements/src/SinCodeSmell/Perro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SinCodeSmell;
+
+/**
+ *
+ * @author Angel Moya
+ */
+public class Perro extends Animal {
+
+    public Perro() {
+        super();
+        setNombre("Perro");
+       
+    }
+
+  
+    
+}
